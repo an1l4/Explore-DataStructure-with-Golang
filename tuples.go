fdb30bb81589efbf3f5d27c4f0e3db4e82e10089
an1l4 <1995anila@gmail.com>
2022-09-05 10:27:38 +0530

    tuples

---
+++ tuples.go
@@ -0,0 +1,16 @@
+//power series of integers are calculated and the square and cube of the integer is returned as a tuple
+
+package main
+
+import "fmt"
+
+func powerSeries(a int) (int, int, error) {
+	return a * a, a * a * a, nil
+
+}
+
+func main() {
+	square, cube := powerSeries(3)
+	fmt.Println("Square: ", square, "Cube: ", cube)
+
+}
