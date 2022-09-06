905e3b1b7d290a8b345e90c433ff1e322415e745
an1l4 <1995anila@gmail.com>
2022-09-06 14:31:29 +0530

    Quadratic-complexity

---
+++ QuadraticComplexity.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println("Multiplication table", i)
+		for j := 1; j <= 10; j++ {
+			x := i * j
+			fmt.Println(x)
+		}
+	}
+
+}
